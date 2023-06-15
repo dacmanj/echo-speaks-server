78ac33fcd259ca3d8134a8b57a44d64f6e149cc1
David Manuel <david@dcmanjr.com>
2023-06-14 22:15:29 -0700

    serverurl

--- index.js
+++ index.js
@@ -382,8 +382,8 @@ function sendServerDataToHub() {
                 body: {
                     version: appVer,
                     onHeroku: configData.settings.useHeroku === true,
-                    isLocal: configData.settings.useHeroku !== true,
-                    serverUrl: configData.settings.useHeroku === true ? null : `http://${getLocalHost()}`,
+                    isLocal: (configData.settings.useHeroku !== true && !process.env.serverUrl),
+                    serverUrl: process.env.serverUrl || (configData.settings.useHeroku === true ? null : `http://${getLocalHost()}`),
                 },
                 json: true,
             };
